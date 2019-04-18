638710fb264d46a1d1866a151e869772649b2bca
Dingo33 <38858711+Dingo33@users.noreply.github.com>
2019-04-18 18:50:06 -0400

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+    int field;
+
+    public test(int field) {
+        this.field = field;
+    }
+
+    public int getField() {
+        return field;
+    }
+
+    public void setField(int field) {
+        this.field = field;
+    }
+}
