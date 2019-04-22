cff07e23328e7e34c74d6320733a7923dfa50ab6
Dingo33 <38858711+Dingo33@users.noreply.github.com>
2019-04-22 17:28:09 -0400

    Add files via upload

---
+++ insertionSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class insertionSort {
+    public static void insertionSort(int[] arr){
+        for(int i = 1; i < arr.length; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6 };
+
+        insertionSort2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void insertionSort2(int[] arr){
+        for(int i = 1; i < arr.length; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+
+    }
+}
